25aa98f5584d72b43d9bbf0cfcdef66897a2d224
RunaBirkeland <runa.birkeland@gmail.com>
2023-01-16 17:22:20 +0100

    Update index.js
    Created my first component as a test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ReactDom.render(<p>Hi, are you there?</p>, document.getElementById("root"))
+function MainContent() {
+    return (
+        <div className="main-content">
+            <h1>This is a test</h1>
+            <p>The goal is to see whether I can create a custom component.</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+<div>
+    <MainContent />
+</div>, 
+document.getElementById("root")
+)
